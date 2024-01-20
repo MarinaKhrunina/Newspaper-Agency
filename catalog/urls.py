27bb5768e61b75b3a8e145ee830dce632e030e34
Marina Khrunina <125975411+MarinaKhrunina@users.noreply.github.com>
2024-01-20 15:55:03 +0100

    modify urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from catalog.views import index
+from catalog.views import (
+    index,
+    TopicListView,
+    NewspaperListView,
+    RedactorListView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
-
+    path("topics/", TopicListView.as_view(), name="topics-list"),
+    path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
+    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
 ]
 
 
