f5df074948a0dab82914b55cc29d52cc07b01218
Marina Khrunina <125975411+MarinaKhrunina@users.noreply.github.com>
2024-01-21 18:37:39 +0100

    Update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,14 +4,21 @@ from catalog.views import (
     index,
     TopicListView,
     NewspaperListView,
+    NewspaperDetailView,
+    NewspaperCreateView,
     RedactorListView,
+    RedactorDetailView,
+    RedactorCreateView,
 )
-
 urlpatterns = [
     path("", index, name="index"),
-    path("topics/", TopicListView.as_view(), name="topics-list"),
+    path("topics/", TopicListView.as_view(), name="topic-list"),
     path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
+    path("newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
+    path("newspapers/create/", NewspaperCreateView.as_view(), name="newspaper-create"),
     path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+    path("redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
+    path("redactors/create/", RedactorCreateView.as_view(), name="redactor-create"),
 ]
 
 
