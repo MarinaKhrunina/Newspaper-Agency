521b99ce703898498cafb046475cd0121a3546f7
Marina Khrunina <125975411+MarinaKhrunina@users.noreply.github.com>
2024-01-21 18:38:10 +0100

    Update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from catalog.models import Newspaper, Redactor, Topic
 
 
+@login_required
 def index(request: HttpRequest) -> HttpResponse:
     num_newspapers = Newspaper.objects.count()
     num_redactors = Redactor.objects.count()
@@ -29,6 +32,27 @@ class NewspaperListView(LoginRequiredMixin, generic.ListView):
     model = Newspaper
 
 
+class NewspaperDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Newspaper
+
+
+class NewspaperCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Newspaper
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:newspaper-list")
+
+
 class RedactorListView(LoginRequiredMixin, generic.ListView):
     model = Redactor
     queryset = Redactor.objects.prefetch_related("newspapers")
+
+
+class RedactorDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Redactor
+
+
+class RedactorCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Redactor
+    fields = "__all__"
+    success_url = reverse_lazy("catalog:redactor-list")
+
