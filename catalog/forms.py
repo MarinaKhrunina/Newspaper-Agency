bb9862bafe7e397539b4cc62610bbd386f87afda
Marina Khrunina <125975411+MarinaKhrunina@users.noreply.github.com>
2024-01-24 20:12:30 +0100

    Update forms.py

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 from catalog.models import Newspaper, Redactor
@@ -8,3 +9,17 @@ class RedactorCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = Redactor
         fields = UserCreationForm.Meta.fields + ("first_name", "last_name", )
+
+
+class NewspaperForm(forms.ModelForm):
+    redactors = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = Newspaper
+        fields = "__all__"
+
+
